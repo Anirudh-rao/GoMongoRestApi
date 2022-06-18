a47e665882333338fba3806396d88cc0198b270b
Anirudh-rao <anirudh131299@gmail.com>
2022-06-18 22:59:08 +0530

    Project Completed

--- main.go
+++ main.go
@@ -3,28 +3,24 @@ package main
 import (
 	"net/http"
 
-	"github.com/Anirudh-rao/GoMongoRestApi/controllers"
-
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
+
+	"github.com/Anirudh-rao/GoMongoRestApi/controllers"
 )
 
 func main() {
-	//Creating Our Basic Router
+
 	r := httprouter.New()
-
 	uc := controllers.NewUserController(getSession())
-
-	//Routes
 	r.GET("/user/:id", uc.GetUser)
-	r.POST("/user/:id", uc.CreateUser)
+	r.POST("/user", uc.CreateUser)
 	r.DELETE("/user/:id", uc.DeleteUser)
-
-	//creating a Local Host
-	http.ListenAndServe("localhost:8080", r)
+	http.ListenAndServe("localhost:9000", r)
 }
 
 func getSession() *mgo.Session {
+
 	s, err := mgo.Dial("mongodb://localhost:27017")
 	if err != nil {
 		panic(err)
